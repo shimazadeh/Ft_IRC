d4d76d19f2f6d57bb780a940c9cd635f2c4cb5c8
Adrien <adrien.guillard@gmail.com>
2023-04-06 20:42:06 +0200

    architerture implementation

--- inc/Tree.hpp
+++ inc/Tree.hpp
@@ -15,28 +15,30 @@ class Tree
         std::unordered_map<std::pair<int, std::string>, User, decltype(hashfunc)>   _m;
 
     public:
+        typedef std::unordered_map<std::pair<int, std::string>, User, decltype(hashfunc)>::iterator iterator;
+
         Tree(): _m(hashfunc) {};
         ~Tree();
         
         User& operator[](const std::string& nickname) 
         {
-            for (auto& p : _m) {
-                if (p.second.get_nickname() == nickname) {
-                    return p.second;
-                }
+            for (iterator p = _m.begin(); p != _m.end(); p++) 
+            {
+                if ((p->second)._nickname == nickname)
+                    return p->second;
             }
-            throw std::runtimeerror("Nickname not found");
+            throw std::runtime_error("Nickname not found");
         }
 
         User& operator[](int fd) {
-            for (auto& p : _m) {
-                if (p.second.get_fd() == fd) {
-                    return p.second;
-                }
+            for (iterator p = _m.begin(); p != _m.end(); p++) 
+            {   
+                if ((p->second)._fd == fd)
+                    return p->second;
             }
             throw std::runtime_error("FD not found");
         }
-        
+
         void    insert(const std::string &nick_name, int fd)
         {
             User user(nick_name, fd);
